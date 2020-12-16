8baa7fbe7c2f6ccde6fbf3d3a2915fb5b012f153
Lili Cohen <lili.cohen.tv@gmail.com>
2020-12-15 22:31:56 -0800

    adding class for employee

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,12 @@
 // TODO: Write code to define and export the Employee class
+
+class Employee {
+  constructor(name, position, id, email) {
+    this.name = name;
+    this.position = position;
+    this.id = id;
+    this.email = email;
+  }
+}
+
+module.exports = Employee;
