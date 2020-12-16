51672626c3bd1334a8c77f0ee66a669ae52c6301
Lili Cohen <lili.cohen.tv@gmail.com>
2020-12-16 00:19:04 -0800

    adding constructor and trying to pass tests for manager

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,12 +1,26 @@
 // TODO: Write code to define and export the Employee class
 
 class Employee {
-  constructor(name, position, id, email) {
+  constructor(name, id, email) {
     this.name = name;
-    this.position = position;
     this.id = id;
     this.email = email;
   }
+    getName() {
+        return this.name;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getRole() {
+        return "Employee";
+    }
 }
 
 module.exports = Employee;
